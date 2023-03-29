347d0ec1b0d28f14d1a91d1a4d9b1cc7a657d4e7
Aziz Mahfoudh <azizmahfoudh359@gmail.com>
2023-03-29 17:13:11 +0200

    Minor change in the formStyle.Css

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -1,21 +1,16 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-import BusinessForm from './components/forms/businessForm'
-import LoginForm from './components/forms/LoginForm'
-import ParticularForm from './components/forms/particularForm'
-import Navbar from './components/homePage/NavBar'
-import OperationBox from './components/homePage/OperationBox'
-import BalanceBox from './components/homePage/BalanceBox'
-import SendPage from './pages/SendPage'
-import Footer from './components/homePage/Footer'
-
+import BusinessForm from "./components/forms/registration/businessForm";
+import LoginForm from "./components/forms/login/LoginForm";
+import ParticularForm from "./components/forms/registration/particularForm";
+import Navbar from "./components/homePage/NavBar";
+import OperationBox from "./components/homePage/OperationBox";
+import BalanceBox from "./components/homePage/BalanceBox";
+import SendPage from "./pages/SendPage";
+import Footer from "./components/homePage/Footer";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <OperationBox heading='Request'></OperationBox>
-  )
+  return <BusinessForm></BusinessForm>;
 }
 
-export default App
+export default App;
